e8655392c62107c8ca6cdf6822cd50f824e77608
Rizqi Setiawan <seiawanrizqi.9@gmail.com>
2025-10-06 10:58:46 +0700

    feat: react-day5

--- week-1/react-day5/Soal-5/vite-5/src/components/Counter.jsx
+++ week-1/react-day5/Soal-5/vite-5/src/components/Counter.jsx
@@ -11,11 +11,29 @@ function Counter() {
     setCount((prevCount) => prevCount - 1);
   };
 
+  const kali = () => {
+    setCount((prevCount) => prevCount * 2);
+  };
+
+  const bagi = () => {
+    setCount((prevCount) => prevCount / 2);
+  };
+
+  const reset = () => {
+    setCount(0);
+  };
+
   return (
-    <div style={{border: "1px solid White", borderRadius: "10px" ,padding: "30px"}}>
-      <p>Count: {count}</p>
-      <button onClick={increment}>Tambah</button>
-      <button onClick={decrement}>Kurang</button>
+    <div style={{background: "#0088ffff", border: "1px solid White", borderRadius: "10px" ,padding: "30px"}}>
+      <h2>Aplikasi Counter Sederhana</h2>
+      <p>Counter: {count}</p>
+      <div style={{ display: "flex", flexDirection: "row", gap: "20px", justifyContent: "center" }}>
+        <button onClick={increment}>+</button>
+        <button onClick={decrement}>-</button>
+        <button onClick={kali}>x</button>
+        <button onClick={bagi}>:</button>
+        <button onClick={reset}>Reset</button>
+      </div>
     </div>
   );
 }
